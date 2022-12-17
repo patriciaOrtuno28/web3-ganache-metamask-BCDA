1d9d54a795431ed9169f8719105a70da5ff990da
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2022-12-17 13:01:17 +0100

    Completed asignatura lite

---
+++ dapp/src/components/evaluaciones/EvaluacionesPage.js
@@ -0,0 +1,12 @@
+import EvaluacionesList from "./EvaluacionesList";
+
+
+const EvaluacionesPage = () => (
+    <section className="AppEvaluaciones">
+        <h2>Evaluaciones</h2>
+
+        <EvaluacionesList/>
+    </section>
+);
+
+export default EvaluacionesPage;
