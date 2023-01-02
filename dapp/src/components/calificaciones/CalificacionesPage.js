ea637735a962e6f344f884548c68dc183777a597
patriciaOrtuno28 <patricia.ortuno28@gmail.com>
2023-01-02 17:53:35 +0100

    Updated 00datos.txt

--- dapp/src/components/calificaciones/CalificacionesPage.js
+++ dapp/src/components/calificaciones/CalificacionesPage.js
@@ -1,16 +1,33 @@
 import CalificacionesTotal from "./CalificacionesTotal";
 import Calificar from "./Calificar";
 import CalificacionEvaluacion from "./CalificacionEvaluacion";
+import {drizzleReactHooks} from "@drizzle/react-plugin";
+
+const {useDrizzle, useDrizzleState} = drizzleReactHooks;
 
 
 const CalificacionesPage = () => {
+
+    // CalificacionesTotal solo se muestra al coordinador o a un profesor
+    const {useCacheCall} = useDrizzle();
+    const coordinador = useCacheCall("Asignatura", "coordinador");
+
+    const state = useDrizzleState(state => state);
+    const address = state.accounts[0];
+
+    var hasPermission = false;
+
+    if (address === coordinador) hasPermission = true;
+
+    const datos = useCacheCall("Asignatura", "datosProfesor", address);
+    if (datos) hasPermission = true;
 
     return (
         <section className="AppCalificaciones">
             <h2>Calificaciones</h2>
 
             <div style= {{display:'flex', flexDirection:'row', justifyContent: 'space-between', marginInline: '0.5rem'}}>
-            <CalificacionesTotal/>
+            {hasPermission && <CalificacionesTotal/>}
             <CalificacionEvaluacion style={{marginInlineEnd: '10rem'}}/>
             </div>
             <Calificar/>
